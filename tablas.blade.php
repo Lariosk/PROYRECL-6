49f01afea1860d2fc24dfae2656d1063ba2b9438
lalo2306 <68831434+lalo2306@users.noreply.github.com>
2022-06-25 18:08:10 -0500

    Add files via upload
    Se agregó 3 archivos

---
+++ tablas.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+<div class="container" id="registration-form">
+<div id="contenedorTabla">
+        <div class="header">
+            <h2>Prestamo</h2>
+        </div>
+        <div id="prestamo">
+        </div>
+        <br>
+
+        <br>
+        <div class="header">
+            <h2>Amortizacion</h2>
+        </div>
+        <div id="amortizaciones">
+        </div>
+    </div>
+</div>
+@endsection
